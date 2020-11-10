427e64ee002d975db516de96ade24dd0f88f82ce
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2020-11-10 21:30:02 +0700

    set ID field

--- controller.go
+++ controller.go
@@ -16,13 +16,14 @@ func InitController(red *redis.Client) Controller {
 	return Controller{Redis: red}
 }
 
+// SHOW DATA
 func (_r *Controller) Show(writer http.ResponseWriter, request *http.Request) {
 	var response Response
 	var user User
 
 	ID := fmt.Sprintf("user:%s", request.URL.Query().Get("id"))
 
-	data, errGetData := _r.Redis.HMGet(context.Background(), ID, "name", "age").Result()
+	data, errGetData := _r.Redis.HMGet(context.Background(), ID, "ID", "name", "age").Result()
 
 	if errGetData != nil {
 		response.Message = errGetData.Error()
@@ -41,8 +42,9 @@ func (_r *Controller) Show(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	user.Name = data[0].(string)
-	user.Age = StrToInt(data[1].(string))
+	user.ID = StrToInt(data[0].(string))
+	user.Name = data[1].(string)
+	user.Age = StrToInt(data[2].(string))
 
 	response.Data = user
 	response.Message = "Success Show Data"
@@ -68,7 +70,7 @@ func (_r *Controller) Store(writer http.ResponseWriter, request *http.Request) {
 
 	ID := fmt.Sprintf("user:%s", IntToStr(user.ID))
 
-	_r.Redis.HSet(context.Background(), ID, "name", user.Name, "age", user.Age)
+	_r.Redis.HSet(context.Background(), ID, "ID", user.ID, "name", user.Name, "age", user.Age)
 
 	response.Data = nil
 	response.Message = "Success Storing Data"
@@ -94,7 +96,7 @@ func (_r *Controller) Update(writer http.ResponseWriter, request *http.Request) 
 
 	key := fmt.Sprintf("user:%s", ID)
 
-	_r.Redis.HSet(context.Background(), key, "name", user.Name, "age", user.Age)
+	_r.Redis.HSet(context.Background(), key, "ID", ID, "name", user.Name, "age", user.Age)
 
 	response.Message = "Success update data"
 	response.Data = nil
@@ -114,7 +116,7 @@ func (_r *Controller) Delete(writer http.ResponseWriter, request *http.Request) 
 
 	key := fmt.Sprintf("user:%s", ID)
 
-	_r.Redis.HDel(context.Background(), key, "name", "age")
+	_r.Redis.HDel(context.Background(), key, "ID", "name", "age")
 
 	response.Message = "Success Delete Data"
 	response.Data = nil
